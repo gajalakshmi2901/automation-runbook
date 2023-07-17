db6d4a9a17dc448dabd053a2e1b0514882af1644
gajalakshmi <gajalakshmi@1cloudhub.com>
2023-07-17 11:12:13 +0530

    commited

--- variable.tf
+++ variable.tf
@@ -1,17 +1,16 @@
 variable "custom_automation_account_name" {
     type=string
-    default="automation-acc"
+   
   
 }
 variable "automation_account_runbook_name"{
     type=string
-    default="runbook"
+    
 }
 variable "location"{
     type=string
-    default="eastus2"
+
 }
 variable "custom_rg_name" {
   type=string
-  default="1chrg"
 }
